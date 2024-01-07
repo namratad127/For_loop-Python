3c332dc16ae49f3b0aceb71e31b9c3dee058f2a0
namratd127 <namratad127@gmail.com>
2024-01-07 20:07:39 +0530

    commit again

--- palindrome pyramid.py
+++ palindrome pyramid.py
@@ -17,3 +17,12 @@ for i in range(1,n+1):
     for l in range(i-1,0,-1):
         print(l,end="")
     print()
+
+'''left alphabetic palindrome pyramid'''
+for i in range(1,7):
+    for j in range(65,65+i):
+        print(chr(j),end="")
+    for k in range(63+i,64,-1):
+        print(chr(k),end="")
+    print()
+
